2fc7ead3c583a37c7fdb63ccb486bd214b9201a8
GOVIND <work.govindsahu@gmail.com>
2025-12-22 14:01:01 +0530

    add-github-actions

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -24,7 +24,7 @@ function DirectoryView({ adminView, isPublic }) {
   const navigate = useNavigate();
 
   // Displayed directory name
-  const [directoryName, setDirectoryName] = useState("My StorageApp");
+  const [directoryName, setDirectoryName] = useState("My Drive");
 
   // Lists of items
   const [directoriesList, setDirectoriesList] = useState([]);
@@ -101,7 +101,7 @@ function DirectoryView({ adminView, isPublic }) {
       setPath(data.path);
 
       // Set directory name
-      setDirectoryName(dirId ? data.name : "My StorageApp");
+      setDirectoryName(dirId ? data.name : "My Drive");
 
       // Reverse directories and files so new items show on top
       setDirectoriesList([...data.directories]);
@@ -126,7 +126,7 @@ function DirectoryView({ adminView, isPublic }) {
       await handleFetchErrors(res);
 
       // Set directory name
-      setDirectoryName(dirId ? data.name : "My StorageApp");
+      setDirectoryName(dirId ? data.name : "My Drive");
 
       // Reverse directories and files so new items show on top
       setDirectoriesList([...data.directories]);
@@ -155,7 +155,7 @@ function DirectoryView({ adminView, isPublic }) {
       setPath(data.path);
 
       // Set directory name
-      setDirectoryName(dirId ? data.name : "My StorageApp");
+      setDirectoryName(dirId ? data.name : "My Drive");
 
       // Reverse directories and files so new items show on top
       setDirectoriesList([...data.directories]);
